34b7d5bea0100e1170a027a7411e74f063e4573a
drew <227942757+deerlyloved@users.noreply.github.com>
2026-03-18 21:21:13 -0700

    Update controller_config.h

--- src/controller_config.h
+++ src/controller_config.h
@@ -2,34 +2,34 @@
 #define CONTROLLER_CONFIG_H
 
 #define SW_GPIO_SIZE 11               // Number of switches
-#define LED_GPIO_SIZE 10              // Number of switch LEDs
-#define ENC_GPIO_SIZE 2               // Number of encoders
-#define ENC_PPR 600                   // Encoder PPR
+#define LED_GPIO_SIZE 11              // Number of switch LEDs
+#define ENC_GPIO_SIZE 1               // Number of encoders
+#define ENC_PPR 360                   // Encoder PPR
 #define MOUSE_SENS 1                  // Mouse sensitivity multiplier
 #define ENC_DEBOUNCE false            // Encoder Debouncing
 #define SW_DEBOUNCE_TIME_US 8000      // Switch debounce delay in us
 #define ENC_PULSE (ENC_PPR * 4)       // 4 pulses per PPR
 #define REACTIVE_TIMEOUT_MAX 1000000  // HID to reactive timeout in us
-#define WS2812B_LED_SIZE 10           // Number of WS2812B LEDs
-#define WS2812B_LED_ZONES 2           // Number of WS2812B LED Zones
+#define WS2812B_LED_SIZE 46           // Number of WS2812B LEDs
+#define WS2812B_LED_ZONES 1           // Number of WS2812B LED Zones
 #define WS2812B_LEDS_PER_ZONE \
   WS2812B_LED_SIZE / WS2812B_LED_ZONES  // Number of LEDs per zone
 
 #ifdef PICO_GAME_CONTROLLER_C
 
 // MODIFY KEYBINDS HERE, MAKE SURE LENGTHS MATCH SW_GPIO_SIZE
-const uint8_t SW_KEYCODE[] = {HID_KEY_D, HID_KEY_F, HID_KEY_J, HID_KEY_K,
-                              HID_KEY_C, HID_KEY_M, HID_KEY_A, HID_KEY_B,
-                              HID_KEY_1, HID_KEY_E, HID_KEY_G};
+const uint8_t SW_KEYCODE[] = {HID_KEY_Z, HID_KEY_S, HID_KEY_X, HID_KEY_D,
+                              HID_KEY_C, HID_KEY_F, HID_KEY_V, HID_KEY_G, 
+                              HID_KEY_ENTER, HID_KEY_TAB, HID_KEY_ESC};
 const uint8_t SW_GPIO[] = {
-    4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 27,
+    16, 14, 12, 10, 8, 6, 4, 18, 22, 24
 };
 const uint8_t LED_GPIO[] = {
-    5, 7, 9, 11, 13, 15, 17, 19, 21, 26,
+    17, 15, 13, 11, 9, 7, 5, 19, 23, 25
 };
-const uint8_t ENC_GPIO[] = {0, 2};      // L_ENC(0, 1); R_ENC(2, 3)
-const bool ENC_REV[] = {false, false};  // Reverse Encoders
-const uint8_t WS2812B_GPIO = 28;
+const uint8_t ENC_GPIO[] = {26}; // (26, 27)
+const bool ENC_REV[] = {false};  // Reverse Encoder
+const uint8_t WS2812B_GPIO = 0;
 
 #endif
 
